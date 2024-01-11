c8f427367e322821a31a72e06811cf665270d170
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2024-01-11 15:11:55 -0600

    new

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -3,25 +3,51 @@ using MySql.Data.MySqlClient;
 using ORM_Dapper;
 using System.Data;
 
-var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-string connString = config.GetConnectionString("DefaultConnection");
-
-IDbConnection conn = new MySqlConnection(connString);
-
-var departmentRepo = new DapperDepartmentRepository(conn);
-
-var department = departmentRepo.GetAllDepartments();
-
-foreach (var departments in department)
+public class Program
 {
-    Console.WriteLine(departments.DepartmentID);
-    Console.WriteLine(departments.Name);
-    Console.WriteLine();
-    Console.WriteLine();
+    private static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 
+        string connString = config.GetConnectionString("DefaultConnection");
+
+        IDbConnection conn = new MySqlConnection(connString);
+
+        #region DEPARTMENT    
+        //var departmentRepo = new DapperDepartmentRepository(conn);
+
+        //var department = departmentRepo.GetAllDepartments();
+
+        //foreach (var departments in department)
+        //{
+        // Console.WriteLine(departments.DepartmentID);
+        // Console.WriteLine(departments.Name);
+        //Console.WriteLine();
+        //Console.WriteLine();
+
+        //}
+        #endregion
+
+
+        var ProductRepo = new DapperDepartmentRepository(conn);
+
+        var products = ProductRepo.GetAllProduct();
+        foreach (var product in products)
+        {
+            Console.WriteLine(product.ProductID);
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Price);
+            Console.WriteLine(product.CategoryID);
+            Console.WriteLine(product.OnSale);
+            Console.WriteLine(product.StockLevel);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
 }
-
---
+++ ORM-Dapper/ProuductRepo.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ORM_Dapper
+{
+  public class ProductRepo : IProductRepository
+    
+{
+    private readonly IDbConnection _conn;
+
+    public ProductRepo(IDbConnection conn)
+    {
+        _conn = conn;
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _conn.Query<Product>("SELECT * FROM products");
+    }
+
+    public void InsertProduct(string name)
+    {
+        _conn.Execute("INSERT INTO products (Name) VALUES (@Name)", new { Name = name });
+    }
+
+    public void CreateProduct(string name, double price, int categoryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        _conn.Execute("UPDATE products" +
+            " SET Name = @name," +
+            " Price = @price," +
+            " CategoryID = @catID," +
+            " OnSale = @onSale," +
+            " StockLevel = @stock" +
+            " WHERE ProductID = @id;",
+            new
+            {
+                id = product.ProductID,
+                name = product.Name,
+                price = product.Price,
+                catID = product.CategoryID,
+                onSale = product.OnSale,
+                stock = product.StockLevel
+            });
+    }
+
+    public void DeleteProduct(Product product)
+    {
+       
+        throw new NotImplementedException();
+    }
+}
+
+}
